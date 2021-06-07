36173b7ae05fcae4bdcf01a43659547f2fb45c5b
Ivan Živković <ivanzivkovic1998@gmail.com>
2021-06-07 23:56:43 +0200

    dodana forma za odabir ishodišta i odredišta

--- view/_header.php
+++ view/_header.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf8">
+	<meta charset="utf-8">
 	<title>Navigacija</title>
 	<link rel="stylesheet" href="<?php echo __SITE_URL;?>/css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
+    <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"></script>
+
+	<style> #mapa {width: 500px, height: 500px;}
+	</style>
 </head>
 <body>
 	<h1><?php echo $title; ?></h1>
--- view/main.php
+++ view/main.php
@@ -2,4 +2,36 @@
 
 <?php echo $loi->__get('name'); ?>
 
+<div id="mapa"></div>
+
+<br/>
+
+<form method="POST" action="<?php echo __SITE_URL . '/index.php?rt=main/search'?>">
+    <input type="radio" name="source_type" id="map_centre" value="map_centre"/>
+    <label for="map_centre">Najbliža točka središtu mape</label>
+    <br/>
+    <input type="radio" name="source_type" id="lat_lon" value="lat_lon"/>
+    <label for="lat_lon">Unesite zemljopisnu širinu i dužinu u stupnjevima:</label>
+    <input type="text" name="text_lat"/>
+    <select name="text_lat_smjer">
+        <option value="N">sjeverno</option>
+        <option value="S">južno</option>
+    </select>
+    <input type="text" name="text_lon"/>
+    <select name="text_lon_smjer">
+        <option value="E">istočno</option>
+        <option value="W">zapadno</option>
+    </select>
+    <br/>
+    <input type="radio" name="source_type" id="name_search" value="name_search"/>
+    <label for="name_search">Pretraživanje po dijelu imena:</label>
+    <input type="text" name="text_name_search"/>
+    <br/>
+
+    <button type="submit" name="add_pt1">Dodaj ishodište!</button>
+    <button type="submit" name="add_pt2">Dodaj odredište!</button>
+    <br/>
+    <button type="submit" name="find_path">Nađi najkraći put!</button>
+</form>
+
 <?php require_once __SITE_PATH . '/view/_footer.php'; ?>
