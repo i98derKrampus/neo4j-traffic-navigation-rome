6a9c27830355a12a2b2a48f7b2e4471ebdc0627a
Mario Marjanovic <rioma101@gmail.com>
2021-06-08 09:29:05 -0700

    quickestpath2

--- model/navigacija.class.php
+++ model/navigacija.class.php
@@ -2,7 +2,7 @@
 
 class Navigacija
 {
-	
+
 	function getTockaById( $id )
 	{
 		$db = DB::getConnection();
@@ -10,9 +10,9 @@ class Navigacija
 		$results = $db->run(
 			'MATCH (n:Tocka) 
 				WHERE n.id = $id
-				RETURN n;', //The query is a required parameter
-			['id' => $id],  //Parameters can be optionally added
-			'backup' //The default connection can be overridden
+				RETURN n;',
+			['id' => $id],  
+			'backup' 
 		);
         $row = false;
 		foreach($results as $item){
@@ -74,14 +74,143 @@ class Navigacija
 			$array_nodes[$i] = $this->getTockaById($node_id);
 			$i++;
 		}
+		$array_distances = $results[0]->get('costs');
+		$total_distane = $results[0]->get('totalDistance');
+		$final_array = array('totalDistance' => $total_distane,
+							'tocke' => $array_nodes,
+							'distances' => $array_distances
+		);
+		return $final_array;
+	}
 
-		return $array_nodes;
+	function getShortestPathAstar($id1, $id2){
+		
+		$db = DB::getConnection();
+		
+		$results = $db->run(
+						'match (source:Tocka {id:$id1}), (target:Tocka {id:$id2})
+						call gds.beta.shortestPath.astar.stream("astartest1",{
+							sourceNode: id(source),
+							targetNode: id(target),
+							latitudeProperty: "latitude",
+							longitudeProperty: "longitude",
+							relationshipWeightProperty: "distance"
+						})
+						yield index, sourceNode, targetNode, totalCost, nodeIds, costs
+						return
+							index,
+							gds.util.asNode(sourceNode).name as sourceNodeName,
+							gds.util.asNode(targetNode).name as targetNodeName,
+							totalCost,
+							[nodeId in nodeIds | gds.util.asNode(nodeId).id] as nodeids,
+							costs
+						order by index',
+						['id1' => $id1, 'id2' => $id2],
+						'backup'
+		);
+
+		$array_nodes_ids = $results[0]->get('nodeids');
+		$array_nodes = array();
+		$i = 0;
+		foreach($array_nodes_ids as $node_id){
+			$array_nodes[$i] = $this->getTockaById($node_id);
+			$i++;
+		}
+		$array_distances = $results[0]->get('costs');
+		$total_distane = $results[0]->get('totalDistance');
+		$final_array = array('totalDistance' => $total_distane,
+							'tocke' => $array_nodes,
+							'distances' => $array_distances
+		);
+		return $final_array;
+
+
+	}
+
+	function getQuickestPathDijkstra($id1, $id2){
+		
+		$db = DB::getConnection();
+		
+		$results = $db->run(
+						'MATCH (source:Tocka {id: $id1}), (target:Tocka {id: $id2})
+						CALL gds.beta.shortestPath.dijkstra.stream("dijkstratest1", {
+							sourceNode: id(source),
+							targetNode: id(target),
+							relationshipWeightProperty: "expected_time"
+						})
+						YIELD index, sourceNode, targetNode, totalCost, nodeIds, costs
+						RETURN
+							index,
+							gds.util.asNode(sourceNode) AS sourceNodeName,
+							gds.util.asNode(targetNode) AS targetNodeName,
+							totalCost,
+							[nodeId IN nodeIds | gds.util.asNode(nodeId).id] AS nodeids,
+							costs
+						ORDER BY index;',
+						['id1' => $id1, 'id2' => $id2],
+						'backup'
+		);
+
+		$array_nodes_ids = $results[0]->get('nodeids');
+		$array_nodes = array();
+		$i = 0;
+		foreach($array_nodes_ids as $node_id){
+			$array_nodes[$i] = $this->getTockaById($node_id);
+			$i++;
+		}
+		$array_distances = $results[0]->get('costs');
+		$total_distane = $results[0]->get('totalTime');
+		$final_array = array('totalDistance' => $total_distane,
+							'tocke' => $array_nodes,
+							'times' => $array_distances
+		);
+		return $final_array;
 	}
 
 
+	function getquickestPathAstar($id1, $id2){
+		
+		$db = DB::getConnection();
+		
+		$results = $db->run(
+						'match (source:Tocka {id:$id1}), (target:Tocka {id:$id2})
+						call gds.beta.shortestPath.astar.stream("astartest1",{
+							sourceNode: id(source),
+							targetNode: id(target),
+							latitudeProperty: "latitude",
+							longitudeProperty: "longitude",
+							relationshipWeightProperty: "expected_time"
+						})
+						yield index, sourceNode, targetNode, totalCost, nodeIds, costs
+						return
+							index,
+							gds.util.asNode(sourceNode).name as sourceNodeName,
+							gds.util.asNode(targetNode).name as targetNodeName,
+							totalCost,
+							[nodeId in nodeIds | gds.util.asNode(nodeId).id] as nodeids,
+							costs
+						order by index',
+						['id1' => $id1, 'id2' => $id2],
+						'backup'
+		);
 
-    
+		$array_nodes_ids = $results[0]->get('nodeids');
+		$array_nodes = array();
+		$i = 0;
+		foreach($array_nodes_ids as $node_id){
+			$array_nodes[$i] = $this->getTockaById($node_id);
+			$i++;
+		}
+		$array_distances = $results[0]->get('costs');
+		$total_distane = $results[0]->get('totalDistance');
+		$final_array = array('totalTime' => $total_distane,
+							'tocke' => $array_nodes,
+							'times' => $array_distances
+		);
+		return $final_array;
 
+
+	}
 
 };
 
