4826f7bf8aca66e9e826a9ec9a70eba5483b9853
Ivan Živković <ivanzivkovic1998@gmail.com>
2021-06-08 18:30:29 +0200

    typo u searchresults

--- view/searchresults.php
+++ view/searchresults.php
@@ -6,6 +6,6 @@
 
 <h2><?php echo $type;?> od ishodišta do odredišta</h2>
 
-<a href="<?php echo __SITE_URL . '/index.php?rt=main'?>">Vrati se na početnu stranicu</a>
+<a href="<?php echo __SITE_URL . '/index.php?rt=main';?>">Vrati se na početnu stranicu</a>
 
 <?php require_once __SITE_PATH . '/view/_footer.php'; ?>
